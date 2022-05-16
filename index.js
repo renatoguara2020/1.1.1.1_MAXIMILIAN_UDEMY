10a127908808e64d9e905945f24bc5cf9e40a3e2
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-16 14:54:42 -0300

    commit changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const sequelize = require('./database/conn')
+const product = require('./models/Product')
 const app = express()
 const port = 3000
 
@@ -8,4 +10,13 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+sequelize.sync().then( result =>{
+//console.log(result);
+app.listen(3001)
+}).catch (err =>{
+
+    console.error(err)
 })
--- models/Product.js
+++ models/Product.js
@@ -34,4 +34,6 @@ description:{
     allowNull: false,
 }
 
-})
+});
+
+module.exports = Product;
